4006c482a4cdd014ab806bff05c6e9c216fcec9f
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-03-17 17:49:32 +0700

    Restructure Pug folder

--- controllers/client/topic.controlles.ts
+++ controllers/client/topic.controlles.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from "express"
 import Topic from "../../models/topic.model"
+
 // [GET]: /topics
 export const index = async (req: Request, res: Response) => {
   const topics = await Topic.find({
     deleted: false
   })
-  res.render("client/pages/topics/index")
+  res.render("client/pages/topics/index",{
+    pageTitle: "Chủ đề bài hát",
+    topics
+  })
 }
