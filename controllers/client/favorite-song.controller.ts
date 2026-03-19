7da31c6aaded79bd5405fd87e22d18c434f342cf
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-03-19 19:50:41 +0700

    Build search result page

--- controllers/client/favorite-song.controller.ts
+++ controllers/client/favorite-song.controller.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express"
-import Topic from "../../models/topic.model"
 import Song from "../../models/song.model"
 import Singer from "../../models/singer.model"
 import FavoriteSong from "../../models/favorite-song.model"
---
+++ helpers/convertToSlug.ts
@@ -0,0 +1,6 @@
+import unidecode from "unidecode"
+export const convertToSlug = (text:string) =>{
+  const unidecodeText = unidecode(text.trim())
+  const slug: string = unidecodeText.replace(/\s+/g, "-")
+  return slug
+}
