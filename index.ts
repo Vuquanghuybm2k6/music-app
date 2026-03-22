83c1f21c4378427b687c8cbcb844bad2f7e76cdf
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-03-22 10:38:19 +0700

    Build admin layout

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import express, { Express, Request, Response } from "express"
-
+import express, { Express } from "express"
+import path from "path"
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -14,8 +14,20 @@ app.use(express.static("public"))
 app.set("views", `${__dirname}/views`)
 app.set("view engine", "pug")
 
+import { systemConfig } from "./config/config";
+app.locals.prefixAdmin = systemConfig.prefixAdmin
+
+// Tiny MCE
+app.use('/tinymce', express.static(path.join(__dirname,'node_modules','tinymce')))
+// End Tiny MCE
+
+
 import clientRoutes from "./routes/client/index.route";
 clientRoutes(app)
+
+import adminRoutes from "./routes/admin/index.route";
+adminRoutes(app)
+
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
