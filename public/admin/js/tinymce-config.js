2d5a1ab696734a82662b3b213a610e991d35d062
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-03-23 17:07:47 +0700

    feat: upload image from tiny mce to cloudinary

--- public/admin/js/tinymce-config.js
+++ public/admin/js/tinymce-config.js
@@ -2,28 +2,29 @@ tinymce.init({
   selector: "textarea.textarea-mce",
   license_key : 'gpl',
   plugins: "image",
+  images_upload_url: '/admin/upload'
   // Upload image 
-  file_picker_callback: function(cb, value, meta) {
-    var input = document.createElement('input');
-    input.setAttribute('type', 'file');
-    input.setAttribute('accept', 'image/*');
+  // file_picker_callback: function(cb, value, meta) {
+  //   var input = document.createElement('input');
+  //   input.setAttribute('type', 'file');
+  //   input.setAttribute('accept', 'image/*');
 
-    input.onchange = function() {
-      var file = this.files[0];
+  //   input.onchange = function() {
+  //     var file = this.files[0];
 
-      var reader = new FileReader();
-      reader.onload = function() {
-        var id = 'blobid' + (new Date()).getTime();
-        var blobCache = tinymce.activeEditor.editorUpload.blobCache;
-        var base64 = reader.result.split(',')[1];
-        var blobInfo = blobCache.create(id, file, base64);
-        blobCache.add(blobInfo);
+  //     var reader = new FileReader();
+  //     reader.onload = function() {
+  //       var id = 'blobid' + (new Date()).getTime();
+  //       var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+  //       var base64 = reader.result.split(',')[1];
+  //       var blobInfo = blobCache.create(id, file, base64);
+  //       blobCache.add(blobInfo);
 
-        cb(blobInfo.blobUri(), { title: file.name });
-      };
-      reader.readAsDataURL(file);
-    };
+  //       cb(blobInfo.blobUri(), { title: file.name });
+  //     };
+  //     reader.readAsDataURL(file);
+  //   };
 
-    input.click();
-  }
+  //   input.click();
+  // }
 })
