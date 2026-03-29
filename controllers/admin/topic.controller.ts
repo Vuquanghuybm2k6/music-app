282b503b6ef52c532c7666f758c6c1b3af38c0c5
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-03-29 11:23:07 +0700

    Delete topic feature (admin)

--- controllers/admin/topic.controller.ts
+++ controllers/admin/topic.controller.ts
@@ -11,3 +11,25 @@ export const index = async (req: Request, res: Response) => {
     topics
   })
 }
+
+// [PATCH]: /admin/topics/delete
+export const deleteTopic = async (req: Request, res: Response) => {
+  const idTopic = req.params.idTopic
+  const topic = await Topic.findOne({
+    _id: idTopic,
+    deleted: false
+  })  
+  if(!topic) {
+    console.log("Không tìm thấy chủ đề")
+    return res.redirect("/admin/topics")
+  }
+  else{
+    await Topic.updateOne({
+      _id: idTopic
+    },{
+      deleted: true
+    })
+    console.log("Xóa chủ đề thành công")
+    return res.redirect("/admin/topics")
+  }
+}
--- routes/admin/topic.route.ts
+++ routes/admin/topic.route.ts
@@ -4,4 +4,6 @@ import * as controller from "../../controllers/admin/topic.controller"
 
 router.get("/", controller.index)
 
+router.patch("/delete/:idTopic", controller.deleteTopic)
+
 export const topicRoutes: Router = router 
