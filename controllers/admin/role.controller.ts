35edce653fba1cecf5a4f2096d22974cb944d120
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-03-30 10:58:31 +0700

    Create role feature

--- controllers/admin/role.controller.ts
+++ controllers/admin/role.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express"
 import Role from "../../models/role.model"
 
-// [GET]: /admin/dashboard
+// [GET]: /admin/roles
 export const index = async (req: Request, res: Response) => {
   const role = await Role.find({
     deleted: false
@@ -10,4 +10,24 @@ export const index = async (req: Request, res: Response) => {
     pageTitle: "Nhóm quyền",
     records: role
   })
+}
+
+// [GET]: /admin/roles/create
+export const create = async (req: Request, res: Response) => {
+  res.render("admin/pages/roles/create",{
+    pageTitle: "Thêm mới Nhóm quyền",
+  })
+}
+
+// [POST]: /admin/roles/create
+export const createPost = async (req: Request, res: Response) => {
+  try{
+    const role = new Role(req.body)
+    await role.save()
+    console.log("Tao mới nhóm quyền thành công")
+    res.redirect("/admin/roles")
+  } catch (error) {
+    console.error("Lỗi khi tạo mới nhóm quyền:", error)
+    res.status(500).send("Đã xảy ra lỗi khi tạo mới nhóm quyền")
+  }
 }
--- routes/admin/role.route.ts
+++ routes/admin/role.route.ts
@@ -3,5 +3,7 @@ const router: Router = Router()
 import * as controller from "../../controllers/admin/role.controller"
 
 router.get("/", controller.index)
+router.get("/create", controller.create)
+router.post("/create", controller.createPost)
 
 export const roleRoutes: Router = router 
